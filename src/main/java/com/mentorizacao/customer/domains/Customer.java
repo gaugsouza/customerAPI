735c6fd4245eec9f7b8f093ab7e536e62aa8eacf
dmartins <dmartins@betait.com.br>
2019-01-29 14:20:52 -0200

    First commit

---
+++ src/main/java/com/mentorizacao/customer/domains/Customer.java
@@ -0,0 +1,67 @@
+package com.mentorizacao.customer.domains;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Version;
+
+import java.io.Serializable;
+
+import java.util.Date;
+
+/**
+ * © Copyright Beta IT 2019<br>
+ *
+ * The <code>Movie</code> class is POJO (Plain Old Java Object) for a set/get of movie (table) from H2 database.
+ *
+ *
+ */
+@AllArgsConstructor
+@Builder
+@Data
+@Entity
+@NoArgsConstructor
+public class Customer extends Object implements Serializable {
+    private static final long serialVersionUID = -5446424219885333861L;
+
+    /** The customer's user name and its primary key*/
+    @Id
+    @GeneratedValue
+    private String userName = null;
+
+    /** The auto-generated version of the customer*/
+    @Version
+    private Integer version  = null;
+
+    /** The customer's first name */
+    private String firstName = null;
+
+    /** The customer's last name */
+    private String lastName = null;
+
+    /** The customer's address */
+    private String address = null;
+
+    /** The customer's city */
+    private String city = null;
+
+    /** The customer's state */
+    private String state = null;
+
+    /** The customer's zip code*/
+    private String zipCode = null;
+
+    /** The customer's password encrypted */
+    private String encryptedPassword= null;
+
+    /** The creation date of the customer */
+    private Date dateCreated = null;
+
+    /** The last updated date of the customer */
+    private Date lastUpdated = null;
+}
